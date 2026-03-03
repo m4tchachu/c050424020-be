97ce7ff15a58419aab1440ffe4d9d6efaba06f35
m4tchachu <sitizahraazizah23@gmail.com>
2026-03-03 15:57:07 +0800

    Penambahan backend

---
+++ app/Filament/Resources/Expenses/Schemas/ExpenseForm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Resources\Expenses\Schemas;
+
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ExpenseForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('amount')
+                    ->required()
+                    ->numeric(),
+                Textarea::make('note')
+                    ->required()
+                    ->columnSpanFull(),
+                DatePicker::make('date_expense')
+                    ->required(),
+                
+            ]);
+    }
+}
---
+++ app/Filament/Resources/Expenses/Tables/ExpensesTable.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\Expenses\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ExpensesTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable(),
+                TextColumn::make('date_expense')
+                    ->date()
+                    ->sortable(),
+                TextColumn::make('amount')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
